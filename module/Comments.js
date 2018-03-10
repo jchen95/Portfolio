cd3f860bda34e1fce4a8573b38993a4365ec8895
Jason Chen <chenjas2@msu.edu>
2018-03-10 16:27:23 -0500

    seperated repos

--- module/Comments.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var commentSchema = new mongoose.Schema({
-  author: String,
-  body: String
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
